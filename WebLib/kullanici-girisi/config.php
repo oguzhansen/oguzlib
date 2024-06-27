470cbb04ec571dacab543adb7d5e5ddfa50d297d
Oğuz Şen <48945284+oguzhansen@users.noreply.github.com>
2024-06-27 14:17:01 +0300

    config.php

--- WebLib/kullanici-girisi/config.php
+++ WebLib/kullanici-girisi/config.php
@@ -1,9 +1,9 @@
 <?php
+    $host = "localhost";
+    $dbname = "blog";  // veritabanı adı
+    $username = "root"; // veritabanı kullanıcıadı
+    $password = ""; // veritabanı kullanıcı şifresi
     try{
-        $host = "localhost";
-        $dbname = "blog";  // veritabanı adı
-        $username = "root"; // veritabanı kullanıcıadı
-        $password = ""; // veritabanı kullanıcı şifresi
         $db = new PDO("mysql:host=$host; dbname=$dbname; charset=utf8","$username","$password");
     }
     catch(PDOException $message){
